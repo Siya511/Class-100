315984c3bb8da23ef11f8d67c04f1786a83d5e98
Siya511 <84263658+Siya511@users.noreply.github.com>
2021-07-19 15:58:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    document.getElementById("textbox").innerHTML = " ";
+    recognition.start();
+}
+
+recognition.onresult = function(event)
+{
+    console.log(event);
+    var content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = content;
+    console.log(content);
+    
+    if(content == "take my selfie")
+    {
+        console.log("Taking Selfie");
+        speak();
+    }
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speakdata = "Taking Your Selfie In 5 Seconds!";
+    var saythis = new SpeechSynthesisUtterance(speakdata);
+
+    synth.speak(saythis);
+    Webcam.attach(camera);
+    setTimeout(function()
+    {
+        snap_shot();
+        save();
+    }, 5000);
+}
+
+camera = document.getElementById("camera");
+
+Webcam.set({
+    width: 360,
+    height: 250,
+    image_format: 'png',
+    png_quality: 90
+});
+
+function snap_shot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML = '<img id = "selfie_image" src="'+data_uri+'">';
+
+    });
+}
+
+function save()
+{
+    link = document.getElementById("link");
+    image = document.getElementById("selfie_image").src;
+    link.href = image;
+    link.click();
+}
